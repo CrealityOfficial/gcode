f85ef3c8c0ba89e980ec3dfb793a28b54833186d
yichenlin <yichenlin@creality.com>
2023-11-22 17:34:00 +0800

    fix[relative extruse is OK]
    Change-Id: I078d62be89aa1a00235cafcb83aebbc27c457eed

--- internal/sliceresult.cpp
+++ internal/sliceresult.cpp
@@ -1,4 +1,5 @@
 ﻿#include "gcode/sliceresult.h"
+#include "gcode/parasegcode.h"
 
 namespace gcode
 {
@@ -122,6 +123,101 @@ namespace gcode
 		fileInfo.close();
 		return true;
     }
+	bool SliceResult::load_pressureEquity(const std::string& fileName, ccglobal::Tracer* tracer)
+	{
+		m_fileName = fileName;
+		m_data_gcodelayer.clear();
+		m_data_gcodeprefix.clear();
+
+		std::ifstream  fileInfo(fileName);
+		if (!fileInfo.is_open())
+		{
+			return false;
+		}
+
+		unsigned long long fileSize = get_file_size(fileInfo);
+		std::string value = "";
+		bool headend = false;
+		unsigned long long int readBytes = 0;
+		std::string temp;
+
+		while (std::getline(fileInfo, temp))
+		{
+			std::vector<std::string> oneLS;
+			Stringsplit(temp, ':', oneLS);
+			if (oneLS.size() == 2 && oneLS.at(0) == ";TYPE")
+		    {
+				if (oneLS.at(1) == "WALL-INNER" || oneLS.at(1) == "WALL-OUTER")
+				{
+					value += (";_EXTRUSION_ROLE:1\n");
+				}
+				if (oneLS.at(1) == "SKIN")
+				{
+				value += (";_EXTRUSION_ROLE:2\n");
+				}
+				if (oneLS.at(1) == "FILL")
+				{
+					value += (";_EXTRUSION_ROLE:3\n");
+				}
+				if (oneLS.at(1) == "SKIRT")
+				{
+					value += (";_EXTRUSION_ROLE:4\n");
+				}
+				if (oneLS.at(1) == "SUPPORT-INTERFACE" || oneLS.at(1) == "SUPPORT")
+				{
+					value += (";_EXTRUSION_ROLE:5\n");
+				}
+			}
+
+			value += (temp + "\n");
+			readBytes += temp.size();
+			if (tracer && (readBytes % 20000) == 0)
+			{
+				float p = (float)((double)readBytes / (double)fileSize);
+				tracer->progress(p);
+
+				if (tracer->interrupt())
+				{
+					tracer->failed("cxLoadGCode load interrupt ");
+					m_data_gcodelayer.clear();
+					break;
+				}
+			}
+
+			if (temp.find(";LAYER_COUNT") != std::string::npos)
+			{
+				if (!headend)
+				{
+					m_data_gcodeprefix.push_back(value);
+				}
+				else
+				{
+					//针对逐个打印 增加抬升代码
+					std::string str = m_data_gcodelayer.back();
+					str += value;
+					m_data_gcodelayer.back() = str;
+				}
+
+				//layerString.append(temp);
+				headend = true;
+				value.clear();
+				continue;
+			}
+			if (!headend)
+			{
+				continue;
+			}
+			if (temp.find(";TIME_ELAPSED:") != std::string::npos)
+			{
+				m_data_gcodelayer.push_back(value);
+				value.clear();
+				continue;
+			}
+		}
+		m_data_gcodetail.push_back(value);
+		fileInfo.close();
+		return true;
+	}
 
 	std::string SliceResult::fileName()
     {
